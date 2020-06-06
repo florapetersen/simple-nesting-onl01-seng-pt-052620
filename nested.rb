598d2f0a11ef49709b6de2e4c954805f43a7a46e
Flora Petersen <floramarina@comcast.net>
2020-06-06 22:45:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie]["":language"]
+		 return programmer_hash[:dennis_ritchie][":language"]
 end
 
 def adding_matz
