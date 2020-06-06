06c7f267b3cd60292664960d7afc0dcbe4e6a99a
Flora Petersen <floramarina@comcast.net>
2020-06-06 22:52:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+		 programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
+		 programmer_hash[:yukihoro_matsumoto][:known_for][:languages] << "LISP", "C"
+		 return programmer_hash
 end
 
 def changing_alan
